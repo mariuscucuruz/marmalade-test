e8a62264b8cc60941469302d84ee3f16b58462e6
Marius Cucuruz <marius.cucuruz@gmail.com>
2022-04-12 19:35:42 +0100

    implement helper for test

--- tests/Tests/Acceptance/AcceptanceTest.php
+++ tests/Tests/Acceptance/AcceptanceTest.php
@@ -11,7 +11,8 @@ class AcceptanceTest extends TestCase
 
     /**
      * @test
-     * The endpoint should accept
+     *
+     * The endpoint accepts GET requests and returns framework version.
      */
     public function api_endpoint_responds_to_get_requests_test()
     {
@@ -28,6 +29,8 @@ class AcceptanceTest extends TestCase
 
     /**
      * @test
+     *
+     * The endpoint rejects PUT requests.
      */
     public function api_endpoint_rejects_put_requests_test()
     {
@@ -38,6 +41,30 @@ class AcceptanceTest extends TestCase
 
         // then
         self::assertEquals(Response::HTTP_METHOD_NOT_ALLOWED, $requestResponse->status());
+    }
+
+    /**
+     * @test
+     *
+     * The endpoint accepts POST requests and returns framework version (no validation yet).
+     *
+     * @throws \JsonException
+     */
+    public function api_endpoint_responds_to_post_requests_test()
+    {
+        // given
+        $requestPayload = $this->makeValidPostRequestPayload();
+
+        // when
+        $requestResponse = $this->call('POST', $this->routeUrl, $requestPayload);
+
+        // then
+        $expected = json_encode([
+            'abi_code' => $this->app->version(),
+        ], JSON_THROW_ON_ERROR);
+
+        self::assertEquals(Response::HTTP_OK, $requestResponse->status());
+        self::assertEquals($expected, $requestResponse->getContent());
     }
 
     /**
@@ -70,5 +97,21 @@ class AcceptanceTest extends TestCase
 
         self::assertEquals(Response::HTTP_OK, $requestResponse->status());
         self::assertEquals($expected, $requestResponse->getContent());
+    }
+
+    /**
+     * @param int    $age
+     * @param string $postcode
+     * @param string $regNo
+     *
+     * @return array
+     */
+    private function makeValidPostRequestPayload(int $age = 20, string $postcode = 'PE3 8AF', string $regNo = 'PJ63 LXR'): array
+    {
+        return [
+            'age'      => $age,
+            'postcode' => $postcode,
+            'regNo'    => $regNo
+        ];
     }
 }
