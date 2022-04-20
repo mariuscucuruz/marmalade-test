2113f707db520f3f9e4e5964a5096df455446fee
Marius Cucuruz <marius.cucuruz@gmail.com>
2022-04-20 15:30:16 +0100

    migrations and seeders

---
+++ database/migrations/30_create_postcode_rating_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class createPostcodeRatingTable extends Migration
+{
+    public const TABLE_NAME = 'postcode_rating';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(self::TABLE_NAME, static function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('postcode_area', 4)->index();
+            $table->double('rating_factor', 10, 2)->nullable();
+            $table->timestamps();
+        });
+
+        #Artisan::call('db:seed', ['--class' => 'PostcodeRatingTableSeeder']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists(self::TABLE_NAME);
+    }
+}
